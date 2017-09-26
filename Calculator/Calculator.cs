7176994f3ed84f91c09ff969eaafe6fdc0fcc8fd
casperv <casp3559@edu.easj.dk>
2017-09-26 09:46:06 +0200

    hhh

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorShared
+{
+    public class Class1
+    {
+
+
+     /// <summary>
+     /// Bruges til at lægge 2 tal sammen.
+     /// </summary>
+     /// <param name="a"></param>
+     /// <param name="b"></param>
+     /// <returns></returns>
+        public double Plus(double a, double b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Bruges til at trække 2 tal fra hinanden.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public double Minus(double a, double b)
+        {
+            return a - b;
+        }
+
+        /// <summary>
+        /// Bruges til at gange 2 tal med hinanden.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public double Gange(double a, double b)
+        {
+            return a*b;
+        }
+
+        /// <summary>
+        /// Bruges til at dividere 2 tal med hinanden.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public double Divider(double a, double b)
+        {
+            return a/b;
+        }
+
+    }
+}
---
+++ CalculatorTests/Class1Tests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorShared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorShared.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        private Calculator _calculator;
+
+        [TestMethod()]
+        public void PlusTest()
+        {
+          Calculator calculator = new Calculator();
+            calculator.Plus(10, 10);
+            Assert.AreEqual(20, calculator.Plus(10,10));
+        }
+
+
+
+        [TestMethod()]
+        public void MinusTest()
+        {
+            Calculator calculator = new Calculator();
+            calculator.Minus(20, 10);
+            Assert.AreEqual(10, calculator.Minus(20,10));
+        }
+
+        [TestMethod()]
+        public void GangeTest()
+        {
+            Calculator calculator = new Calculator();
+            calculator.Gange(5, 5);
+            Assert.AreEqual(25, calculator.Gange(5,5));
+        }
+
+        [TestMethod()]
+        public void DividerTest()
+        {
+            Calculator calculator = new Calculator();
+            calculator.Divider(10, 2);
+            Assert.AreEqual(5, calculator.Divider(10,2));
+        }
+    }
+}
